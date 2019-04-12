63110d028b4dd86e6d2b1f731740a34d99f1d8f3
ArcaneDiver <michele.dellamea@gmail.com>
2019-04-12 20:57:09 +0200

    new file:   indexImage.js 	modified:   subIndexImage.js 	modified:   index.js 	modified:   view/index.ejs

--- subIndexImage.js
+++ subIndexImage.js
@@ -1,24 +1,55 @@
 var ledMatrix = require('easybotics-rpi-rgb-led-matrix');
-
+var child = require('child_process');
 var matrix = new ledMatrix(32, 64, 1, 4, 20);
 var fs = require('fs');
 
 while(1){
 	
-	var x = 256;
-	const imageBuff = fs.readFileSync("b.ppm");
-	matrix.setImageBuffer(imageBuff, 256, 32);
-	while (x > (-256)) { // 26 perche ci sono gli spazi prima
+	var x = 0;
+	var convert = child.spawnSync('convert', ['ciao.jpg', 'input.ppm']);
+	
+	var imgBuff = fs.readFileSync('input.ppm');
+	
+	var imgLength = imgBuff.length - 14;
+
+
+	var openToRead = fs.openSync('input.ppm', 'r');
+	var openToWrite = fs.openSync('output.ppm', 'w');
+	console.log(openToRead, openToWrite);
+	var realBuff = new Buffer(imgLength);
+
+	fs.readSync(openToRead, realBuff, 0, imgLength, 14, callBack);
+	//delay(5);
+	//console.log(realBuff);
+	fs.writeSync(openToWrite, realBuff, 0, imgLength, 0, callBack);
+	//delay(5);
+
+	const imageBuff = fs.readFileSync("output.ppm");
+	//console.log(realBuff.length, imageBuff.length);
+	//console.log(imageBuff.length, imageBuff);
+	var width, height;
+	height = 32; //questo fa preso come assodato perche al momento non so come calcolarne le dimensioni
+	width = (imageBuff.length / 3 ) / height;
+	
+	//console.log(height, width, imageBuff.length);
+	
+	matrix.brightness(20);//rimettere arrText[4]
+	console.log(width, height);
+	matrix.setImageBuffer(imageBuff, width, height);
+	while(1){
+		x=0;
+	while (x<width) {
 		
 		matrix.clear();	
-		matrix.brightness(50);//rimettere arrText[4]
 		
-		matrix.draw();	
+		matrix.draw(0, 0, 256, 32, x, 0, true, false);	
 		matrix.update();
-		x--;
+		x++;
 		
 		delay(0);
-	}
+	}}
+	fs.close(openToRead);
+	fs.close(openToWrite);
 
 }
 function delay(ms){
@@ -31,4 +62,11 @@ function delay(ms){
 		ms_passed = ticks - cur_ticks;
 	}
 	
+}
+function removeHeader(i, o){
+	var i = 0;
+	
+}
+var callBack = function(err, bytesRead, buffer){
+	console.log('err: ', err);
 }
